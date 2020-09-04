21d86241dec02f8ee92ff22f5607e13b21868ade
Fatima Rizvi <fatimarzv23@gmail.com>
2020-09-04 10:20:52 -0500

    Completed header.js mvp

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,29 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    //Instantiating elements
+    const pageHeader = document.createElement('div')
+    const date = document.createElement('span')
+    const pageTitle = document.createElement('h1')
+    const temp = document.createElement('span')
+    //Setting class names, attributes, and text
+    //Attributes and text
+    date.textContent = 'MARCH 28, 2020'
+    pageTitle.textContent = 'Lambda Times'
+    temp.textContent = '98°'
+    //Class names
+    pageHeader.classList.add('header')
+    date.classList.add('date')
+    temp.classList.add('temp')
+    //Creating the hierarchy
+    pageHeader.appendChild(date)
+    pageHeader.appendChild(pageTitle)
+    pageHeader.appendChild(temp)
+
+    return pageHeader
+}
+
+const headerContainer = document.querySelector('.header-container');
+const myHeader = Header()
+headerContainer.appendChild(myHeader);
